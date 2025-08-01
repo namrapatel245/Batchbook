104ff23e0d029f92061a3239470400e921e3e33d
SANNARI <namrapatel244@gmail.com>
2025-08-01 10:46:29 +0530

    first commit3

--- db.js
+++ db.js
@@ -1,9 +1,14 @@
 require('dotenv').config(); // 👈 must be at the top
 
 const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
 
 const connectToMongo = () => {
   console.log("MONGO_URI =", process.env.MONGO_URI); // for debugging
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Batchbook backend listening on port ${port}`);
+});
 
   mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
